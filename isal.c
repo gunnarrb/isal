5587955daca5b9df012e80357450c6ede109bf98
Ragnar Gisli Olafsson <ragnargisli@tgad04.rhi.hi.is>
2011-04-18 21:25:15 +0000

    added variables and constants

--- isal.c
+++ isal.c
@@ -10,6 +10,8 @@
 #include "simlib/rndlib.h"
 #include "simlib/simlib.h"
 
+// EVENTS
+
 #define EVENT_WAGEN_ARRIVAL	1
 #define EVENT_WAGEN_DEPARTURE 2
 #define EVENT_SKAUT_ARRIVAL 3
@@ -19,15 +21,24 @@
 #define EVENT_END_SIMULATION 7
 #define EVENT_END_WARMUP	8
 
+//Other constants
+
+#define NUM_MACHINES 7
+#define NUM_QUEUES 5 // vid flokkum lasa einnig sem bidradir
+
+// Global variables
+int fjoldi_vela, lengd_bidrada, min_afkost_per_dag,lagmarksfjoldi_bilanna_per_day, hamarksfjold_bilanna_per_day;
+float vinnutimar_vela,  mean_wagen_arrival, std_wagen_arrival, mean_bilanir, std_bilanir, min_vidgerdartimi_vela, max_vidgerdartimi_vela, end_warmup_timi, end_hermun_timi;
+
 // What streams do we need?
 
 /* Model variables */
 int num_machines, num_queues;
-int machine2queue[num_machines +1], 
-	is_machine_busy[num_machines +1],
-	queue_sizes[num_queues +1];
+int machine2queue[NUM_MACHINES +1], 
+	is_machine_busy[NUM_MACHINES +1],
+	queue_sizes[NUM_QUEUES +1];
 	
-float work_time[num_stations + 1] // +1 is the less preferable simlib indexing scheme
+float work_time[NUM_MACHINES + 1] // +1 is the less preferable simlib indexing scheme
 FILE *infile, *outfile;
 
 /* Function signatures */
@@ -44,7 +55,7 @@ void skaut_departure();
 //			global variables from the input file exist.
 // Post:	the global variables were assigned values from input_filename, 
 //			the variables along with their values were written to output_filename
-void parse_input();
+void parse_input(char[] inputfile, char[] outputfile);
 
 void generate_report();
 
@@ -62,3 +73,13 @@ int main()
 	
 }
 
+void parse_input(char[] inputfile_data, char[] inputfile_time, char[] outputfile)
+{
+  infile = fopen (inputfile_data, "r");
+  outfile = fopen (outputfile, "w");
+  
+  /* Read input parameters. */
+  fscanf (infile, "%d %d %f %f %f", &min_tellers, &max_tellers, &mean_interarrival, &mean_service, &length_doors_open);
+
+
+}
