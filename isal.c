6ed054caf2688f1ffcffafbb1d800ef2a992591c
gunnarrb <gunnarrb@gmail.com>
2011-04-18 19:33:37 +0000

    progress

--- isal.c
+++ isal.c
@@ -10,22 +10,29 @@
 #include "simlib/rndlib.h"
 #include "simlib/simlib.h"
 
-#define EVENT_ARRIVAL	1
-#define EVENT_DEPARTURE 2
-#define EVENT_END_SIMULATION 3
-#define EVENT_END_WARMUP	4
+#define EVENT_WAGEN_ARRIVAL	1
+#define EVENT_WAGEN_DEPARTURE 2
+#define EVENT_SKAUT_ARRIVAL 3
+#define EVENT_SKAUT_DEPARTURE 4
+#define EVENT_MACHINE_FAILURE 5
+#define EVENT_MACHINE_FIXED 6
+#define EVENT_END_SIMULATION 7
+#define EVENT_END_WARMUP	8
 
-// Do we nee streams for arrivals or failures perhaps? */
+// What streams do we need?
 
 /* Model variables */
-int num_stations, num_queues;
+int num_machines, num_queues;
+int machine2queue[num_machines +1], is_machine_busy[num_machines +1];
 float work_time[num_stations + 1] // +1 is the less preferable simlib indexing scheme
 FILE *infile, *outfile;
 
 /* Function signatures */
-void arrive();
-void departure();
-void report();
+void wagen_arrival();
+void wagen_departure();
+void skaut_arrival();
+void skaut_departure();
+void generate_report();
 
 int main()
 {
