4d414ff8f2e202093f08eeaed6e133f6b9012d61
Ragnar Gisli Olafsson <ragnargisli@tgad06.rhi.hi.is>
2011-04-19 18:01:18 +0000

    datamanipulating and reading data added

--- parse_input.c
+++ parse_input.c
@@ -1,19 +1,21 @@
 void parse_input(char inputfile_data[], char inputfile_time[], char outputfile[])
 {
-	  infile = fopen (inputfile_data, "r");
-	    outfile = fopen (outputfile, "w");
-		  
-		  /* Read input parameters. */
-		  fscanf (infile, "%d %d %d %d %d %f %f %f %f %f %f %f %f %f",   &fjoldi_vela, &lengd_bidrada, &min_afkost_per_dag, &lagmarksfjoldi_bilanna_per_day, &hamarksfjold_bilanna_per_day, &vinnutimar_vela, &mean_wagen_arrival, &std_wagen_arrival, &mean_bilanir, &std_bilanir, &min_vidgerdartimi_vela, &max_vidgerdartimi_vela, &end_warmup_timi, &end_hermun_timi);
+  infile = fopen (inputfile_data, "r");
+  outfile = fopen (outputfile, "w");
+  
+ 
+  /* Read input parameters. */
+  fscanf (infile, " %d %d %d %d %f %f %f %f %f %f %f %f %f", &number_of_machiens, &min_productivity, &min_no_failures, &max_no_failures, &mean_wagen_arrival, &std_wagen_arrival, &mean_failures, &std_failures, &min_machine_repair_time, &max_machine_repair_time, &end_warmup_time, &end_simulation_time);
+  fclose (infile);
+  infile = fopen (inputfile_time, "r");
+  
+  int counter = 1;
+  while (!feof(infile)) {
+    fscanf(infile, "%f %d", &work_time[counter], &queue_size[counter] );
+    counter++;
+  }
+  
+  close(infile);
 
-		    fclose (infile);
-			  infile = fopen (inputfile_time, "r");
-			   
-			    int counter = 1;
-				  while (!feof(infile)) {
-					      fscanf(infile, "%f %d", &work_time[counter], &queue_size[counter] );
-						      counter++;
-							    }
 
-				    close(infile);
 }
